192ee80151a1f42b86be0b14dbea3204289971e0
Leszek Rudnicki P <rmrm2002>
2025-05-01 17:22:34 +0200

    Recommendation dependant on weekday 1) UNTESTED - Now any movie should be only recommended on Sunday

--- lib/pages/movie_details/movie_details_page.dart
+++ lib/pages/movie_details/movie_details_page.dart
@@ -2,14 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:flutter_recruitment_task/models/movie_details.dart';
 import 'package:intl/intl.dart';
 
+import '../../utils/now_inject.dart';
+
 // TODO refactor? MovieDetailsPage to StatelessWidget?
 
 class MovieDetailsPage extends StatefulWidget {
+  final NowInject nowInject;
   final String title;
   final String budget;
   final String revenue;
 
   const MovieDetailsPage(
+    this.nowInject,
     this.title,
     this.budget,
     this.revenue, {
@@ -31,8 +35,10 @@ class MovieDetailsPageState extends State<MovieDetailsPage> {
     super.initState();
     _title = widget.title;
     _details = [
-      MovieDetails(label: 'Budget', content: _makeDollarAmountString(widget.budget)),
-      MovieDetails(label: 'Revenue', content: _makeDollarAmountString(widget.revenue)),
+      MovieDetails(label: 'Budget', content: _makeDollarAmountString('500')),
+      MovieDetails(label: 'Revenue', content: _makeDollarAmountString('5000000')),
+      // MovieDetails(label: 'Budget', content: _makeDollarAmountString(widget.budget)),
+      // MovieDetails(label: 'Revenue', content: _makeDollarAmountString(widget.revenue)),
       MovieDetails(label: 'Should I watch it today?', content: _getIsWorthwhile()),
     ];
   }
@@ -40,17 +46,22 @@ class MovieDetailsPageState extends State<MovieDetailsPage> {
   String _makeDollarAmountString(String amountString) {
     var amount = int.parse(amountString);
 
-    if (amount <= 0) return '\$ 0';
-
     // debug only
     // amount += (Random().nextDouble() * 3000000 + 2000000).round();
+
+    if (amount <= 0) return '\$ 0';
+
     return formatter.format(amount);
   }
 
   String _getIsWorthwhile() {
+    var isSunday = widget.nowInject.weekday() == 7;
+
     var revenue = int.parse(widget.revenue);
     var budget = int.parse(widget.budget);
-    return (revenue - budget) > _interestingProfits ? 'Yes!' : 'No...';
+    var isProfitSatisfactory = (revenue - budget) > _interestingProfits;
+
+    return isSunday && isProfitSatisfactory ? 'Yes!' : 'No...';
   }
 
   @override
