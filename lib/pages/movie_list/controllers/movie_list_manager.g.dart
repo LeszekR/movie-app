c7674f56b85bcfe7dd36ef3514fcf44f88179ae6
Leszek Rudnicki P <rmrm2002>
2025-05-04 14:12:16 +0200

    Added generated files to repo

---
+++ lib/pages/movie_list/controllers/movie_list_manager.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'movie_list_manager.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$movieListManagerHash() => r'55a0a1945ac434712e4dba5ecd2a5a6f68b09c62';
+
+/// See also [MovieListManager].
+@ProviderFor(MovieListManager)
+final movieListManagerProvider =
+    AutoDisposeNotifierProvider<MovieListManager, MovieListManager>.internal(
+  MovieListManager.new,
+  name: r'movieListManagerProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$movieListManagerHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$MovieListManager = AutoDisposeNotifier<MovieListManager>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
