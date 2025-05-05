7d5aa32212c8987122a7bdbf9a02417e1d194166
Leszek Rudnicki P <rmrm2002>
2025-05-05 14:56:17 +0200

    GitLab pipeline correction - missing giles added to repo

---
+++ lib/pages/movie_list/state/movie_list_state.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'movie_list_state.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$movieListStateHash() => r'772ec95a3c9321474d11b2282d0330919141c012';
+
+/// See also [MovieListState].
+@ProviderFor(MovieListState)
+final movieListStateProvider =
+    NotifierProvider<MovieListState, MovieListView>.internal(
+  MovieListState.new,
+  name: r'movieListStateProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$movieListStateHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$MovieListState = Notifier<MovieListView>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
