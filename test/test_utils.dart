b9bb3efba1c3113a2abc9ad1d546d95a6c7e8148
Leszek Rudnicki P <rmrm2002>
2025-10-01 15:23:24 +0200

    chore(deps): update intl from ^0.19.0 to ^0.20.2

--- test/test_utils.dart
+++ test/test_utils.dart
@@ -1,6 +1,5 @@
 import 'dart:io';
 
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_recruitment_task/ui_localized_texts/app_localizations/app_localizations.dart';
